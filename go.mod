8c7caf602fec8e0e96739d9377c54e0dcd13ab24
M Hickford <mirth.hickford@gmail.com>
2022-10-15 05:59:53 +0100

    pkce

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/hickford/git-credential-oauth
 
 go 1.19
 
-require golang.org/x/oauth2 v0.0.0-20221006150949-b44042a4b9c1
+require (
+	go.pinniped.dev v0.20.0
+	golang.org/x/oauth2 v0.0.0-20221006150949-b44042a4b9c1
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/net v0.0.0-20220923203811-8be639271d50 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -13,6 +13,7 @@ import (
 	"os/exec"
 	"strings"
 
+	"go.pinniped.dev/pkg/oidcclient/pkce"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 	"golang.org/x/oauth2/gitlab"
@@ -21,8 +22,8 @@ import (
 var configByHost = map[string]oauth2.Config{
 	// borrowed from GCM
 	// TODO: create own
-	"github.com": {ClientID: "0120e057bd645470c1ed", ClientSecret: "18867509d956965542b521a529a79bb883344c90", Endpoint: github.Endpoint},
-	"gitlab.com": {ClientID: "172b9f227872b5dde33f4d9b1db06a6a5515ae79508e7a00c973c85ce490671e", ClientSecret: "f7b0fe4d82bc3c770b22b55cff60528fe52a859b", Endpoint: gitlab.Endpoint},
+	"github.com": {ClientID: "0120e057bd645470c1ed", ClientSecret: "18867509d956965542b521a529a79bb883344c90", Endpoint: github.Endpoint, Scopes: []string{"repo"}},
+	"gitlab.com": {ClientID: "172b9f227872b5dde33f4d9b1db06a6a5515ae79508e7a00c973c85ce490671e", ClientSecret: "7da92770d1447508601e4ba026bc5eb655c8268e818cd609889cc9bae2023f39", Endpoint: gitlab.Endpoint, Scopes: []string{"read_repository", "write_repository"}},
 }
 
 func main() {
@@ -42,7 +43,7 @@ func main() {
 			log.Fatal(err)
 		}
 	case "get":
-		raw, err := ioutil.ReadAll(os.Stdin)
+		raw, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -82,8 +83,14 @@ func main() {
 		}
 		c.RedirectURL = server.URL
 		// workaround for GCM app
-		c.RedirectURL = strings.ReplaceAll(server.URL, "127.0.0.1", "localhost")
-		url := c.AuthCodeURL(state)
+		if pairs["host"] == "github.com" {
+			c.RedirectURL = strings.ReplaceAll(server.URL, "127.0.0.1", "localhost")
+		}
+		pcode, err := pkce.Generate()
+		if err != nil {
+			log.Fatal(err)
+		}
+		url := c.AuthCodeURL(state, pcode.Challenge(), pcode.Method())
 		fmt.Fprintf(os.Stderr, "Please complete authentication in your browser %s\n", url)
 		err = exec.Command("open", url).Run()
 		if err != nil {
@@ -91,7 +98,7 @@ func main() {
 		}
 		code := <-codes
 		server.Close()
-		token, err := c.Exchange(context.Background(), code)
+		token, err := c.Exchange(context.Background(), code, pcode.Verifier())
 		if err != nil {
 			log.Fatal(err)
 		}
