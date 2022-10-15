e943ba639253929de569457a4ce8b1f133748b90
M Hickford <mirth.hickford@gmail.com>
2022-10-15 16:41:30 +0100

    add gitlab.freedesktop.org

--- main.go
+++ main.go
@@ -26,7 +26,8 @@ var configByHost = map[string]oauth2.Config{
 	// https://github.com/settings/applications/2017944 owned by hickford
 	"github.com": {ClientID: "b895675a4e2cf54d5c6c", ClientSecret: "2b746eea028711749c5062b9fe626fed78d03cc0", Endpoint: github.Endpoint, Scopes: []string{"repo", "gist", "workflow"}},
 	// https://gitlab.com/oauth/applications/232663 owned by hickford
-	"gitlab.com": {ClientID: "10bfbbf46e5b760b55ce772a262d7a0205eacc417816eb84d37d0fb02c89bb97", ClientSecret: "e1802e0ac361efc72f8e2024e6fd5855bfdf73524b67740c05e755f55b97eb39", Endpoint: gitlab.Endpoint, Scopes: []string{"read_repository", "write_repository"}},
+	"gitlab.com":             {ClientID: "10bfbbf46e5b760b55ce772a262d7a0205eacc417816eb84d37d0fb02c89bb97", ClientSecret: "e1802e0ac361efc72f8e2024e6fd5855bfdf73524b67740c05e755f55b97eb39", Endpoint: gitlab.Endpoint, Scopes: []string{"read_repository", "write_repository"}},
+	"gitlab.freedesktop.org": {ClientID: "ba28f287f465c03c629941bca9de965923c561f8e967ce02673a0cd937a94b6f", ClientSecret: "e3b4dba6e99a0b25cc3d3d640e418d6cc5dbeb2e2dc4c3ca791d2a22308e951c", Endpoint: replaceHost(gitlab.Endpoint, "gitlab.freedesktop.org"), Scopes: []string{"read_repository", "write_repository"}},
 }
 
 func main() {
@@ -86,10 +87,6 @@ func main() {
 		}))
 		defer server.Close()
 		c.RedirectURL = server.URL
-		// workaround for GCM app
-		if c.ClientID == "0120e057bd645470c1ed" {
-			c.RedirectURL = strings.ReplaceAll(server.URL, "127.0.0.1", "localhost")
-		}
 		pcode, err := pkce.Generate()
 		if err != nil {
 			log.Fatal(err)
@@ -124,4 +121,14 @@ func randomString(n int) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(data), nil
+}
+
+func replaceHost(e oauth2.Endpoint, host string) oauth2.Endpoint {
+	url, err := url.Parse(e.AuthURL)
+	if err != nil {
+		panic(err)
+	}
+	e.AuthURL = strings.Replace(e.AuthURL, url.Host, host, 1)
+	e.TokenURL = strings.Replace(e.TokenURL, url.Host, host, 1)
+	return e
 }
