32f1d977fd2132db98d18b4a02efe37015d6b64f
M Hickford <mirth.hickford@gmail.com>
2022-10-18 05:08:02 +0100

    gitlab.gnome.org support

--- main.go
+++ main.go
@@ -27,8 +27,10 @@ var configByHost = map[string]oauth2.Config{
 	// https://gitlab.com/oauth/applications/232663 owned by hickford
 	"gitlab.com":             {ClientID: "10bfbbf46e5b760b55ce772a262d7a0205eacc417816eb84d37d0fb02c89bb97", ClientSecret: "e1802e0ac361efc72f8e2024e6fd5855bfdf73524b67740c05e755f55b97eb39", Endpoint: endpoints.GitLab, Scopes: []string{"read_repository", "write_repository"}},
 	"gitlab.freedesktop.org": {ClientID: "ba28f287f465c03c629941bca9de965923c561f8e967ce02673a0cd937a94b6f", ClientSecret: "e3b4dba6e99a0b25cc3d3d640e418d6cc5dbeb2e2dc4c3ca791d2a22308e951c", Endpoint: replaceHost(endpoints.GitLab, "gitlab.freedesktop.org"), Scopes: []string{"read_repository", "write_repository"}},
-	"gitea.com":              {ClientID: "e13f8ebc-398d-4091-9481-5a37a76b51f6", ClientSecret: "gto_gyodepoilwdv4g2nnigosazr2git7kkko3gadqgwqa3f6dxugi6a", Endpoint: oauth2.Endpoint{AuthURL: "https://gitea.com/login/oauth/authorize", TokenURL: "https://gitea.com/login/oauth/access_token"}},
-	"bitbucket.org":          {ClientID: "abET6ywGmTknNRvAMT", ClientSecret: "df8rsnkAxuHCgZrSgu5ykJQjrbGVzT9m", Endpoint: endpoints.Bitbucket, Scopes: []string{"repository", "repository:write"}},
+	// https://gitlab.gnome.org/oauth/applications/112 owned by hickford
+	"gitlab.gnome.org": {ClientID: "9719f147e6117ef0ee9954516bd7fe292176343a7fd24a8bcd5a686e8ef1ec71", ClientSecret: "f4e027961928ba9322fd980f5c4ee768dc7b6cb8fd7a81f959feb61b8fdec9f3", Endpoint: replaceHost(endpoints.GitLab, "gitlab.gnome.org"), Scopes: []string{"read_repository", "write_repository"}},
+	"gitea.com":        {ClientID: "e13f8ebc-398d-4091-9481-5a37a76b51f6", ClientSecret: "gto_gyodepoilwdv4g2nnigosazr2git7kkko3gadqgwqa3f6dxugi6a", Endpoint: oauth2.Endpoint{AuthURL: "https://gitea.com/login/oauth/authorize", TokenURL: "https://gitea.com/login/oauth/access_token"}},
+	"bitbucket.org":    {ClientID: "abET6ywGmTknNRvAMT", ClientSecret: "df8rsnkAxuHCgZrSgu5ykJQjrbGVzT9m", Endpoint: endpoints.Bitbucket, Scopes: []string{"repository", "repository:write"}},
 }
 
 var (
