7c6e4e6b6297a284aceea4bcffb3764006ae960f
M Hickford <mirth.hickford@gmail.com>
2022-10-18 20:45:43 +0100

    support more forges

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	for key, c := range configByHost {
+		if !strings.Contains(c.Endpoint.AuthURL, key) {
+			t.Errorf("bad auth url for key %s: %s", key, c.Endpoint.AuthURL)
+		}
+		if !strings.Contains(c.Endpoint.TokenURL, key) {
+			t.Errorf("bad token url for key %s: %s", key, c.Endpoint.TokenURL)
+		}
+	}
+}
